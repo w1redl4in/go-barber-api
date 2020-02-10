47ea595afee8200cf680ea5b506a9573f7789f24
Felipe <felipe1180@hotmail.com>
2020-02-09 21:19:40 -0300

    endpoint validation with yup

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,13 +1,26 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(6)
+    })
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: "Some fields are missing!" })
+    }
+
     const userExists = await User.findOne({ where: { email: req.body.email } });
     if (userExists) {
       return res.status(400).json({ error: 'User already exists' });
     }
     const { id, name, email, provider } = await User.create(req.body);
     return res.json({ id, name, email, provider });
+
   }
 
   async index(req, res) {
@@ -16,6 +29,23 @@ class UserController {
   }
 
   async update(req, res) {
+
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().required().min(6),
+      password: Yup.string().min(6).when('oldPassword', (oldPassword, field) =>
+        oldPassword ? field.required() : field
+      ),
+      confirmPassword: Yup.string().min(6).when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      )
+    })
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: "Some fields are missing or have invalid values!" })
+    }
+
     const { email, oldPassword } = req.body
 
     const user = User.findByPk(req.userId)
