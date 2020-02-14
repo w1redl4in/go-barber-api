00547b8920b62b9b4cad54729039e87355d0cc62
Felipe <felipe.araujo@hubfintech.com.br>
2020-02-14 14:01:24 -0300

    notifications for provider appointments

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,7 +1,9 @@
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import Notification from '../schemas/Notification';
 import * as Yup from 'yup';
 
 class AppointmentController {
@@ -52,6 +54,19 @@ class AppointmentController {
       provider_id,
       date,
     });
+
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', ás' H:mm'h'",
+      { locale: pt }
+    );
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para o ${formattedDate}`,
+      user: provider_id,
+    });
+
     return res.json(appointment);
   }
 
