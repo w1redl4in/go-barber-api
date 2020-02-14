c940d2b436778972ba24378055c09055721164d6
Felipe <felipe.araujo@hubfintech.com.br>
2020-02-14 13:02:08 -0300

    pagination for appointments

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -56,6 +56,7 @@ class AppointmentController {
   }
 
   async index(req, res) {
+    const { page = 1 } = req.query;
     const appointments = await Appointment.findAll({
       where: {
         user_id: req.userId,
@@ -63,6 +64,8 @@ class AppointmentController {
       },
       order: ['date'],
       attributes: ['id', 'date'],
+      limit: 20,
+      offset: (page - 1) * 20,
       include: [
         {
           model: User,
