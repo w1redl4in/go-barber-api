289818e314ecf1844cacf4a10878a842e1eb44e4
Felipe <felipe.araujo@hubfintech.com.br>
2020-02-14 08:49:05 -0300

    added business rule for appointments

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,5 +1,6 @@
 import Appointment from '../models/Appointment';
 import User from '../models/User';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import * as Yup from 'yup';
 
 class AppointmentController {
@@ -23,6 +24,26 @@ class AppointmentController {
       return res
         .status(401)
         .json({ error: 'You can only create appointments with providers' });
+    }
+
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted' });
+    }
+
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not available' });
     }
 
     const appointment = await Appointment.create({
