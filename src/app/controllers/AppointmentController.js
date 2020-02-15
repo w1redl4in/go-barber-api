2abb363348f56d46a52381faa4b0cf6b592cf281
Felipe <felipe1180@hotmail.com>
2020-02-15 11:49:10 -0300

    delete appointments

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,6 +1,6 @@
 import pt from 'date-fns/locale/pt';
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
@@ -35,7 +35,7 @@ class AppointmentController {
       },
     });
 
-    if (isTheSameProvider) {
+    if (isTheSameProvider === req.userId) {
       return res
         .status(400)
         .json({ error: 'You cannot schedule an appointment with yourself' });
@@ -109,6 +109,28 @@ class AppointmentController {
     });
 
     return res.json(appointments);
+  }
+
+  async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id);
+    if (appointment.user_id !== req.userId) {
+      return res.status(401).json({
+        error: "You don't have permission to cancel this appointment.",
+      });
+    }
+
+    const dateWithSub = subHours(appointment.date, 2);
+
+    if (isBefore(dateWithSub, new Date())) {
+      res
+        .status(401)
+        .json({ error: 'You can only cancel appointments 2 hours in advance' });
+    }
+
+    appointment.canceled_at = new Date();
+    await appointment.save();
+
+    return res.json(appointment);
   }
 }
 
